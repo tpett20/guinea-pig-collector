60f20ba2fb5e189627d0d04095e593f3ef36877c
tpett20 <thomaspettersen20@gmail.com>
2023-04-11 14:58:59 -0400

    create placeholder about page and header navbar, which successfully load.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+guinea_pigs = [
+  {'name': 'Bernie', 'color': 'brown, white, and black', 'description': 'loves to eat cucumbers', 'age': 5},
+  {'name': 'Kodai', 'color': 'tan', 'description': 'loves to popcorn', 'age': 2},
+]
+
 # Create your views here.
+def about(request):
+    return render(request, 'about.html')
+
+def guinea_pigs_index(request):
+    return render(request, 'guinea-pigs/index.html', {
+        'guinea pigs': guinea_pigs
+    })
