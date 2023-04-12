1b69aeca746ddaf23ab4432d7a426b3d7d6de3fc
tpett20 <thomaspettersen20@gmail.com>
2023-04-11 20:07:56 -0400

    additional Bootstrap styling

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 guinea_pigs = [
   {'name': 'Bernie', 'color': 'brown, white, and black', 'description': 'loves to eat cucumbers', 'age': 5},
   {'name': 'Kodai', 'color': 'tan', 'description': 'loves to popcorn', 'age': 2},
+  {'name': 'Squirrel', 'color': 'brown', 'description': 'loves to climb on her house', 'age': 4}
 ]
 
 # Create your views here.
